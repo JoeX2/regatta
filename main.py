dc668d094e2ca293f0f4d21eecbd843f610e4dc9
Joe Sørensen <joe.sorensen@gmail.com>
2016-08-09 22:41:07 +0200

    Added a lot of buttons

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty, StringProperty
 
 class RegattaTimeTable(Widget):
     pass
 
 class RegattaApp(App):
     def build(self):
-        return RegattaTimeTable();
+        r = RegattaTimeTable()
+        
+        return r
 
 if __name__ == '__main__':
     RegattaApp().run()
