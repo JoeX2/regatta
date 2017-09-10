0b39396bd93050e3ff098ff21f8e8650e1a564ff
Joe Sørensen <joe.sorensen@gmail.com>
2017-09-10 10:49:26 +0200

    Added top button

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import StringProperty
+from kivy.lang import Builder
 from kivy.logger import Logger
 
+class RegattaScreenManager(ScreenManager):
+    pass
 
-class RegattaTimeTable(Widget):
+class TimeRecordScreen(Screen):
     label_text = StringProperty( "Text to change" )
 
     def buttonCallback(self):
         self.label_text = str( "Text changed" )
-        Logger.info("Hello5")
-
+        Logger.info("HomeScreen buttonCallback")
 
 class RegattaApp(App):
     def build(self):
-        r = RegattaTimeTable()
-        return r
+        return TimeRecordScreen()
 
+def main():
+    app = RegattaApp()
+    app.run()
 
 if __name__ == '__main__':
-    RegattaApp().run()
+    main()
